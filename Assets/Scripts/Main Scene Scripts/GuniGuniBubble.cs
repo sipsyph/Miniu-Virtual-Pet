c0cf98a8db9440efa78a0b5f39e4c71b8192d337
Jance <jancep@sxi.com.ph>
2021-01-17 16:03:48 +0800

    Changed guniGuniBubble object and shouldDisable bool var access to static. Added method for making the guniGuni bubble active (so it can be seen when called)

--- Assets/Scripts/Main Scene Scripts/GuniGuniBubble.cs
+++ Assets/Scripts/Main Scene Scripts/GuniGuniBubble.cs
@@ -6,26 +6,33 @@ public class GuniGuniBubble : MonoBehaviour {
 
     Quaternion rotation;
     public GameObject guniGuniBubble;
+    public static GameObject guniGuniBubbleStatic;
     public TextMesh textInBubble1, textInBubble2;
     public static string textToBeDisplayed1, textToBeDisplayed2;
-    private bool shouldDisable;
+    public static bool shouldDisable;
 
 	// Use this for initialization
 	void Start () {
+        guniGuniBubbleStatic = guniGuniBubble;
         shouldDisable = false;
-        textToBeDisplayed1 = "";
-        textToBeDisplayed2 = "";
-        //InvokeRepeating("DisableGuniGuniObject", 6, 6);
 	}
 	
 	// Update is called once per frame
 	void Update () {
+        textInBubble1.text = textToBeDisplayed1;
+        textInBubble2.text = textToBeDisplayed2;
 		if(guniGuniBubble.active && !shouldDisable)
         {
             shouldDisable = true;
             Invoke("DisableGuniGuniObject", 6);
         }
 	}
+    
+    public static void ShowGuniGuni()
+    {
+        shouldDisable = false;
+        guniGuniBubbleStatic.SetActive(true);
+    }
 
     void Awake()
     {
@@ -34,8 +41,7 @@ public class GuniGuniBubble : MonoBehaviour {
 
     void LateUpdate()
     {
-        textInBubble1.text = textToBeDisplayed1;
-        textInBubble2.text = textToBeDisplayed2;
+        
         transform.rotation = rotation;
     }
 
